9004f728f40bb1f7aef0f761d8d4803be838f3c7
Lzdanowicz <Lzdanowicz10@gmail.com>
2015-03-18 11:38:30 -0500

    debug

--- js/jazz.js
+++ js/jazz.js
@@ -12,17 +12,20 @@
 
 
 function playSomeSound(genre){
-SC.get('/tracks',{
-    genre: genre,
+SC.get(
+  '/tracks',
+  {
+    genres:genre,
     bpm:{
       from:100
     }
-  }, function(tracks){
+  },
+  function(tracks){
     var random=Math.floor(Math.random()*49);
-    SC.oEmbed(tracks[random]).uri,{auto_play:true}, document.getElementById('target'));
+    SC.oEmbed(tracks[random].uri,{auto_play:true}, document.getElementById('target'));
   };
   );
-}
+};
 
 
 
