e7adea42f1e76442318c112ac051be9432c72b10
Lzdanowicz <Lzdanowicz10@gmail.com>
2015-03-18 15:11:12 -0500

    another update

--- js/jazz.js
+++ js/jazz.js
@@ -10,33 +10,71 @@
 // request.send();
 
 
+ function playSomeSound(genre) {
+    SC.get('/tracks', { genres: genre, limit: 100 }, function(tracks){
+      playRandomTrack(tracks);
+      playlist = tracks;
+    })
+  }
+
+  function playRandomTrack(tracks) {
+    var randomTrack = tracks[Math.floor(Math.random() * tracks.length)];
+    embedTrack(randomTrack);
+  }
+
+    function embedTrack(pickedTrack) {
+    SC.oEmbed(pickedTrack.permalink_url, {auto_play: true, maxheight: 260, color: "#DF3939"}, document.getElementById('target'));
+  }
 
 
-function playSomeSound(genre){
-SC.get(
-  '/tracks',
-    {
-    genres:genre,
-    limit:100
-  },
-  function(tracks){
-    var random=Math.floor(Math.random()*49);
-    SC.oEmbed(tracks[random].permalink_url, {auto_play:true}, document.getElementsByClassName('target'));
+  $("#playButton").click(function(e){
+    e.preventDefault();
+    playSomeSound("jazz"); // also e.currentTarget
+    pickRandomTrack();
+    embedTrack(randomTrack);
   });
-};
 
 
 
-window.onload=function(){
-SC.initialize({
-  client_id: '472ea9cffc056ea4f5bc762b4bc00655',
-});
-var link = document.getElementById('playButton');
-link.onclick = function(e){
-  e.preventDefault();
-  playSomeSound("jazz");
-  };
-};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function playSomeSound(genre){
+// SC.get(
+//   '/tracks',
+//     {
+//     genres:genre,
+//     limit:100
+//   },
+//   function(tracks){
+//     var random=Math.floor(Math.random()*49);
+//     SC.oEmbed(tracks[random].permalink_url, {auto_play:true}, document.getElementsByClassName('target'));
+//   });
+// };
+
+
+
+// window.onload=function(){
+// SC.initialize({
+//   client_id: '472ea9cffc056ea4f5bc762b4bc00655',
+// });
+// var link = document.getElementById('playButton');
+// link.onclick = function(e){
+//   e.preventDefault();
+//   playSomeSound("jazz");
+//   };
+// };
 
 
 
