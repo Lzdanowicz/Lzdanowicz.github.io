f33b784eb706f22db0653b9f13f0ebad1d171937
Lzdanowicz <Lzdanowicz10@gmail.com>
2015-03-18 11:21:17 -0500

    new update

--- js/jazz.js
+++ js/jazz.js
@@ -1,13 +1,13 @@
-var request = new XMLHttpRequest();
+// var request = new XMLHttpRequest();
 
-request.onreadystatechange = function () {
-  if (request.readyState === 4 && request.status === 200) {
-    console.log(request.responseText);
-  }
-};
+// request.onreadystatechange = function () {
+//   if (request.readyState === 4 && request.status === 200) {
+//     console.log(request.responseText);
+//   }
+// };
 
-request.open('GET', 'http://api.soundcloud.com/tracks?client_id=472ea9cffc056ea4f5bc762b4bc00655');
-request.send();
+// request.open('GET', 'http://api.soundcloud.com/tracks?client_id=472ea9cffc056ea4f5bc762b4bc00655');
+// request.send();
 
 
 
