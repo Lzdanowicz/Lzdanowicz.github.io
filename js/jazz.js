8300dda5e9f359344fef8dc4e4b7bf824848441c
Lzdanowicz <Lzdanowicz10@gmail.com>
2015-03-18 11:36:07 -0500

    new update

--- js/jazz.js
+++ js/jazz.js
@@ -19,8 +19,9 @@ SC.get('/tracks',{
     }
   }, function(tracks){
     var random=Math.floor(Math.random()*49);
-    SC.oEmbed(tracks[random]).uri,{auto_play:true}, document.getElementById('target')
-  });
+    SC.oEmbed(tracks[random]).uri,{auto_play:true}, document.getElementById('target'));
+  };
+  );
 }
 
 
